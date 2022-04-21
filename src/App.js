03c735d05223dfa3acf6754f4803b9c1e69e12e2
Łukasz Serafin <lukser1337@gmail.com>
2022-04-21 13:32:29 +0200

    added new h1 into App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const App = () => {
   const [number,setNumber] = useState(2137);
   return (
     <div>
+      <h1>jelo mordo jestem leszek</h1>
       {number}
       <button onClick={()=>getRandomNumber()}>JEBAC KOLARZA</button>
     <input type="number" value={sentence} onChange={(e)=>setSentence(e.target.value)} />
